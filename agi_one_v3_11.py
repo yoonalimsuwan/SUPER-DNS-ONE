70d6d81ed95b2bf5cd7f24f69aeec1864282fdb8
PAI AND Yoon A Limsuwan (美美) <msps4u@gmail.com>
2026-07-25 04:41:04 +0700

    Update agi_one_v3_11.py

--- agi_one_v3_11.py
+++ agi_one_v3_11.py
@@ -494,7 +494,7 @@
 #
 # =============================================================================
 # MIT License
-# Copyright (c) 2026 Yoon A Limsuwan / MSPS NETWORK
+# Copyright (c) 2026 PAI , Yoon A Limsuwan / MSPS NETWORK
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
 # to deal in the Software without restriction, including without limitation
