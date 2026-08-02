706d5501ee3a12e54989d5d3974aa3a807ccbdf1
PAI AND Yoon A Limsuwan (美美) <msps4u@gmail.com>
2026-08-02 19:16:52 +0700

    Update developer information in comments

--- bv_formulated_4vector_potential_maxwell_structural_bridges.py
+++ bv_formulated_4vector_potential_maxwell_structural_bridges.py
@@ -2,7 +2,7 @@
 # BV-FORMULATED 4-VECTOR POTENTIAL MAXWELL-STRUCTURAL BRIDGE
 # EVOLUTION ONE Cluster / ONE Ecosystem
 # =============================================================================
-# Developer    : Yoon A Limsuwan / MSPS NETWORK
+# Developer    : PAI , Yoon A Limsuwan / MSPS NETWORK
 # ORCID        : 0009-0008-2374-0788
 # GitHub       : yoonalimsuwan
 # Contact      : msps4u@gmail.com
