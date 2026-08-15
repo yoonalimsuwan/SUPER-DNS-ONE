d4e795ce06a46c61c299b036379f4408b8f141a3
Mr.PAI And Mrs.Yoon A (Mei Mei) (美美) / Yoon A Limsuwan <msps4u@gmail.com>
2026-08-15 17:27:26 +0700

    Update sesi_ontogenetic_optical_neuro_modulation_module.py

--- sesi_ontogenetic_optical_neuro_modulation_module.py
+++ sesi_ontogenetic_optical_neuro_modulation_module.py
@@ -1,3 +1,17 @@
+# =============================================================================
+# Production-Grade Native Differentiable Optogenetics & Optical Neuromodulation Module (SESI)
+# SUPER DNS ONE Cluster / ONE Ecosystem
+# =============================================================================
+# =============================================================================
+# Developer    : PAI , Yoon A Limsuwan / MSPS NETWORK
+# ORCID        : 0009-0008-2374-0788
+# GitHub       : yoonalimsuwan
+# Contact      : msps4u@gmail.com
+# License      : MIT
+# Year         : 2026
+# =============================================================================
+
+
 """
 Production-Grade Native Differentiable Optogenetics & Optical Neuromodulation Module
 Incorporate Self-Evolving Structural Interfaces (SESI), Disordered Energy Landscapes,
